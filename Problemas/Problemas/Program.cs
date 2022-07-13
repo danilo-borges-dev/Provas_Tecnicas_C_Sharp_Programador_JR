7980d47358d566fb800b358bff282ef46c7cac8e
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-07-13 17:44:47 -0300

    Problema 02 contar a quantidade de caracteres em uma string resolvido com sucesso

--- Problemas/Problemas/Program.cs
+++ Problemas/Problemas/Program.cs
@@ -3,6 +3,7 @@
 static void Main()
 {
     //Problema_01_InverterUmaString();
+    Problema_02_ContarCaracteres();
 }
 
 // 01 - Inverta a string gorila
@@ -37,6 +38,45 @@ static void Problema_02_ContarCaracteres()
 {
     string text = "iausioAAduhsdjqaaaadssdsSJIOadLkkkkass295sdaa4d";
     char caracter = 'a';
+    int counter = 0;
 
+    for (int i = text.Length; i > 0; i--)
+    {
+        if (text[i - 1] == caracter)
+        {
+            counter++;
+        }
+    }
+    Console.WriteLine($"Exite {counter} vezes o caractere 'a' na string.");
 
+    Console.WriteLine();
+    // Caso considerar A e a, podemos:
+    caracter = 'A';
+    counter = 0;
+    text = text.ToUpper();
+    for (int i = text.Length; i > 0; i--)
+    {
+        if (text[i - 1] == caracter)
+        {
+            counter++;
+        }
+    }
+    Console.WriteLine($"Exite {counter} vezes o caractere 'a' na string.");
+
+    Console.WriteLine();
+    // Alternativa para resolução do problema
+    counter = 0;
+    foreach (var c in text)
+    {
+        if (c == caracter)
+        {
+            counter++;
+        }
+    }
+    Console.WriteLine($"Exite {counter} vezes o caractere 'a' na string.");
+
+    Console.WriteLine();
+    // Alternativa para resolução do problema
+    int n = text.Where(c => c == caracter).Count();  // Utilizando o método Where de Link
+    Console.WriteLine($"Exite {n} vezes o caractere 'a' na string.");
 }
