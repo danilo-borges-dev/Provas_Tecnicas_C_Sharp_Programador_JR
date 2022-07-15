5f5523c1453d10fd94c8c5fe2e375264696b88f3
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-07-15 10:54:28 -0300

    Praticando os exercicios

--- Problemas/Problemas/Program.cs
+++ Problemas/Problemas/Program.cs
@@ -14,7 +14,12 @@ static void Main()
     //{
     //    Console.WriteLine(ex.Message);
     //}
-    Resolver_04_ContasPalavras();
+    //Resolver_04_ContasPalavras();
+    //Resolver_05_ContasNumeros();
+    //Exercicio_01();
+    //Exercicio_03();
+    //Exercicio_04();
+    Exercicio_05();
 }
 
 // 01 - Inverta a string gorila
@@ -44,7 +49,7 @@ static void Resolver_01_InverterUmaString()
     Console.WriteLine(result);
 }
 
-// 02 - Conte quantas vezes se repete um "a" caractere
+// 02 - Conte quantas vezes se repete o "a" caractere
 static void Resolver_02_ContarCaracteres()
 {
     string text = "iausioAAduhsdjqaaaadssdsSJIOadLkkkkass295sdaa4d";
@@ -97,7 +102,7 @@ static void Resolver_02_ContarCaracteres()
 // é o número de posições nas quais elas diferem entre si.
 // Vista de outra forma, ela corresponde ao menos número de
 // substituições necessárias para tranformar uma string
-// na outra, ou o número de erros que transforma uma na outra.)
+// na outra, ou o número de erros necessário para transformar uma na outra.)
 static void Resolver_03_DistanciaDeHamming()
 {
     // Para realizar este tipo de medição, contagem, as duas
@@ -138,4 +143,97 @@ static void Resolver_04_ContasPalavras()
     n = words.Length;
 
     Console.WriteLine($"Números de palavras: {n}");
+}
+
+// 05 - Contas números em uma string
+static void Resolver_05_ContasNumeros()
+{
+    string text = "an18jq???daº895,748598mAnsIIpo";
+    string pattern = @"[0-9]";
+    var regex = new Regex(pattern);
+    int n = regex.Matches(text).Count;
+    Console.WriteLine("Total de números = " + n);
+
+
+    string pattern_02 = @"[a-zA-Z]";
+    regex = new Regex(pattern_02);
+    int l = regex.Matches(text).Count;
+    Console.WriteLine("Total de letras = " + l);
+}
+
+// Praticando
+static void Exercicio_01()
+{
+    Console.WriteLine("gorila");
+    string text = "gorila";
+    string result = null;
+
+    for (int i = text.Length; i > 0; i--)
+    {
+        result += text[i - 1];
+    }
+
+    Console.WriteLine(result);
+
+    Console.WriteLine();
+
+    Console.WriteLine("gorila");
+    char[] textChar = text.ToCharArray();
+    Array.Reverse(textChar);
+    Console.WriteLine(textChar);
+}
+static void Exercicio_02()
+{
+    string text = "Amanhã é um novo dia para vencer na vida";
+    int count = 0;
+
+    foreach (var c in text)
+    {
+        if (c == 'a' || c == 'A')
+        {
+            count++;
+        }
+    }
+    Console.WriteLine($"Total de letras a ou A = {count}");
+
+    char ctr = 'a';
+    int n = text.Where((x) => x == ctr).Count();
+    Console.WriteLine($"Total de letras a = {n}");
+}
+static void Exercicio_03()
+{
+    string text = "salvador";
+    string text2 = "sauvator";
+    int n = 0;
+
+    for (int i = 0; i < text.Length; i++)
+    {
+        if (text[i] != text2[i])
+        {
+            n++;
+        }
+    }
+    Console.WriteLine("Números de erros: " + n);
+}
+static void Exercicio_04()
+{
+    string text = "  um texto    tem quantas palavras      ";
+    int n = 0;
+    string patter = @"\s+";
+
+    text = text.Trim();
+    text = Regex.Replace(text, patter, " ");
+    Console.WriteLine(text);
+    var words = text.Split(" ");
+    n = words.Length;
+    Console.WriteLine(n);
+}
+static void Exercicio_05()
+{
+    string text = "anbjk85pooasd9214AsdfsP64548";
+    string patter = @"[0-9]";
+    //var regex = Regex.Matches(patter, text).Count;
+    var regex = new Regex(patter);
+    int l = regex.Matches(text).Count;
+    Console.WriteLine("Total de números : {0}", l);
 }
